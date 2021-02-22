cec1150e8d380c462accd83e0627f07388664849
Tawika Nyathi <tnyathi@singular.co.za>
2021-02-22 21:43:09 +0200

    Axios change.

--- src/Utils/apiHandler/axiosInstance.ts
+++ src/Utils/apiHandler/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig} from 'axios'
 
-const axiosConfig: AxiosRequestConfig = { baseURL: "http://localhost:80"};
+const axiosConfig: AxiosRequestConfig = { baseURL: "http://localhost"};
 
 const axiosInstance = axios.create(axiosConfig);
 
